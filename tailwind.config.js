50d0c2bfe6aa70880e0bbdc8fe2b9af79e0038e4
suszynskiventures-create <suszynskiventures@gmail.com>
2025-11-06 23:23:41 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,10 @@ export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: { extend: {} },
   plugins: [],
+  // keep gradient utilities used by the background blobs
+  safelist: [
+    {
+      pattern: /(from|via|to)-(indigo|fuchsia|blue|sky)-(400|500)\/(10|20)/,
+    },
+  ],
 };
